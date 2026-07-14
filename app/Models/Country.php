f9550129c39975268e8d010da3ffbcfa58b009f8
sharvin1718 <sharvins659@gmail.com>
2026-07-14 16:33:13 +0530

    sharvin finish till migration and model

---
+++ app/Models/Country.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Country extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = [
+        'country_name'
+    ];
+
+    public function states()
+    {
+        return $this->hasMany(State::class);
+    }
+}
