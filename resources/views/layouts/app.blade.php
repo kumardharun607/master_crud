bcb5b29368672927a3a8e58899628f6fe5d07ea4
sharvin1718 <sharvins659@gmail.com>
2026-07-14 17:53:46 +0530

    sharvin craete app blade

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <title>Master</title>
+
+    <script src="https://cdn.tailwindcss.com"></script>
+
+</head>
+
+<body class="bg-gray-100">
+
+<div class="flex">
+
+    <!-- Sidebar -->
+
+    <div class="w-64 min-h-screen bg-gray-800 text-white">
+
+        <h2 class="text-center text-2xl font-bold py-6 border-b">
+
+            MASTER
+
+        </h2>
+
+        <ul class="mt-5">
+
+            <li>
+                <a href="/country"
+                class="block px-6 py-3 hover:bg-gray-700">
+                    Country
+                </a>
+            </li>
+
+            <li>
+                <a href="#"
+                class="block px-6 py-3 hover:bg-gray-700">
+                    State
+                </a>
+            </li>
+
+            <li>
+                <a href="#"
+                class="block px-6 py-3 hover:bg-gray-700">
+                    City
+                </a>
+            </li>
+
+            <li>
+                <a href="#"
+                class="block px-6 py-3 hover:bg-gray-700">
+                    Pincode
+                </a>
+            </li>
+
+        </ul>
+
+    </div>
+
+    <!-- Content -->
+
+    <div class="flex-1 p-10">
+
+        @yield('content')
+
+    </div>
+
+</div>
+
+</body>
+
+</html>
