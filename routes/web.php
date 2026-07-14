df3fea1dcd67d524b295a60ddec665a3ac3440e4
sharvin1718 <sharvins659@gmail.com>
2026-07-14 18:22:27 +0530

    sharvin's country crud is finished

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CountryController;
 
+// Redirect Home
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/country');
 });
+
+// Country CRUD
+Route::get('/country', [CountryController::class, 'index']);
+
+Route::post('/country/store', [CountryController::class, 'store']);
+
+Route::get('/country/edit/{id}', [CountryController::class, 'edit']);
+
+Route::post('/country/update/{id}', [CountryController::class, 'update']);
+
+Route::delete('/country/delete/{id}', [CountryController::class, 'destroy']);
