7d099142d4554a14a137a87da157c34f002172ad
dharunkumar <kumardharun607@gmail.com>
2026-07-14 18:31:45 +0530

    dharun changes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,20 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\StateController;
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/state', [StateController::class, 'index']);
+
+Route::post('/state', [StateController::class, 'store']);
+
+Route::get('/state/list', [StateController::class, 'list']);
+
+Route::get('/state/{id}/edit', [StateController::class, 'edit']);
+
+Route::put('/state/{id}', [StateController::class, 'update']);
+
+Route::delete('/state/{id}', [StateController::class, 'destroy']);
